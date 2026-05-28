0dc48a1ad12af37e26c844124a2977fa9bc94c77
Samantha Cohn <samcohn20@gmail.com>
2026-05-27 19:09:40 -0500

    Auto-commit [May 27 19:09]

--- overview.js
+++ overview.js
@@ -858,7 +858,7 @@ function tRowVidVirt(t,arr){
   </div>`;
 }
 function _pupSessStyle(){
-  return{bg:'#f7fbff',b:'rgba(100,160,230,.25)',t:'#2c5ea0',d:'#5a9de0',dot:'rgba(195,222,255,.20)'};
+  return{bg:'#f0f8ff',b:'rgba(100,170,220,.25)',t:'#2c5ea0',d:'#5a9de0',dot:'rgba(170,210,240,.25)'};
 }
 function _pupDisplayName(t){const p=t._pup;return p?(p+': '+(t.name||'')):(t.name||'');}
 function tRowPupSess(t,noColor=false,tbArrow=false){
